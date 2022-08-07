e7a54a82b7216e60ca7515441c4e6dd065b2f33b
Hacking Devil <shobhanpakhira@gmail.com>
2022-08-07 18:10:09 +0530

    Formatted the file

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
-'use strict';
+"use strict";
 
 /* Add Event On Element */
 const addEventOnElem = function (elem, type, callback) {
-    if (elem.length > 1) {
-      for (let i = 0; i < elem.length; i++) {
-        elem[i].addEventListener(type, callback);
-      }
-    } else {
-      elem.addEventListener(type, callback);
-    }
-  }
+	if (elem.length > 1) {
+		for (let i = 0; i < elem.length; i++) {
+			elem[i].addEventListener(type, callback);
+		}
+	} else {
+		elem.addEventListener(type, callback);
+	}
+};
 
-/* Navbar Toggle */ 
+/* Navbar Toggle */
 const navbar = document.querySelector("[data-navbar]");
 const navbarLinks = document.querySelectorAll("[data-nav-link]");
 const navToggler = document.querySelector("[data-nav-toggler]");
 
 const toggleNavbar = function () {
-  navbar.classList.toggle("active");
-  navToggler.classList.toggle("active");
-  document.body.classList.toggle("active");
-}
+	navbar.classList.toggle("active");
+	navToggler.classList.toggle("active");
+	document.body.classList.toggle("active");
+};
 
 addEventOnElem(navToggler, "click", toggleNavbar);
 
 const closeNavbar = function () {
-  navbar.classList.remove("active");
-  navToggler.classList.remove("active");
-  document.body.classList.remove("active");
-}
+	navbar.classList.remove("active");
+	navToggler.classList.remove("active");
+	document.body.classList.remove("active");
+};
 
 addEventOnElem(navbarLinks, "click", closeNavbar);
 
-/* Header Active*/ 
+/* Header Active*/
 
 const header = document.querySelector("[data-header]");
 
-const activeHeader = function() {
-    if(window.scrollY > 300){
-    header.classList.add("[active]");
-    } else {
-        header.classList.remove("active");
-    }
-}
+const activeHeader = function () {
+	if (window.scrollY > 300) {
+		header.classList.add("[active]");
+	} else {
+		header.classList.remove("active");
+	}
+};
 
 addEventOnElem(window, "scroll", activeHeader);
